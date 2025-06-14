3a90d58810cbf5d0c019f2eaa9f28f42e4d5f379
Daniel <20250019@ricaldone.edu.sv>
2025-06-14 11:41:08 -0600

    Cambio

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,8 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.gbPrincipal = new System.Windows.Forms.GroupBox();
             this.lblError = new System.Windows.Forms.Label();
-            this.btnLimpiar = new System.Windows.Forms.Button();
-            this.btnComprar = new System.Windows.Forms.Button();
             this.lblCambio = new System.Windows.Forms.Label();
             this.txtCambio = new System.Windows.Forms.TextBox();
             this.txtDinero = new System.Windows.Forms.TextBox();
@@ -40,6 +38,8 @@
             this.lblDinero = new System.Windows.Forms.Label();
             this.lblCosto = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnComprar = new System.Windows.Forms.Button();
             this.gbPrincipal.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,11 +55,11 @@
             this.gbPrincipal.Controls.Add(this.txtCosto);
             this.gbPrincipal.Controls.Add(this.lblDinero);
             this.gbPrincipal.Controls.Add(this.lblCosto);
-            this.gbPrincipal.Location = new System.Drawing.Point(201, 81);
-            this.gbPrincipal.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.gbPrincipal.Location = new System.Drawing.Point(222, 58);
+            this.gbPrincipal.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.gbPrincipal.Name = "gbPrincipal";
-            this.gbPrincipal.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.gbPrincipal.Size = new System.Drawing.Size(446, 217);
+            this.gbPrincipal.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.gbPrincipal.Size = new System.Drawing.Size(573, 275);
             this.gbPrincipal.TabIndex = 0;
             this.gbPrincipal.TabStop = false;
             this.gbPrincipal.Text = "Pantalla Principal";
@@ -68,91 +68,106 @@
             // 
             this.lblError.AutoSize = true;
             this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Location = new System.Drawing.Point(211, 176);
+            this.lblError.Location = new System.Drawing.Point(359, 193);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(39, 30);
+            this.lblError.Size = new System.Drawing.Size(45, 38);
             this.lblError.TabIndex = 8;
             this.lblError.Text = "Error\r\n\r\n";
-            // 
-            // btnLimpiar
-            // 
-            this.btnLimpiar.Location = new System.Drawing.Point(280, 129);
-            this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
-            this.btnLimpiar.TabIndex = 7;
-            this.btnLimpiar.Text = "Limpiar";
-            this.btnLimpiar.UseVisualStyleBackColor = true;
-            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
-            // 
-            // btnComprar
-            // 
-            this.btnComprar.Location = new System.Drawing.Point(280, 70);
-            this.btnComprar.Name = "btnComprar";
-            this.btnComprar.Size = new System.Drawing.Size(75, 23);
-            this.btnComprar.TabIndex = 6;
-            this.btnComprar.Text = "Comprar";
-            this.btnComprar.UseVisualStyleBackColor = true;
-            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
             // 
             // lblCambio
             // 
             this.lblCambio.AutoSize = true;
-            this.lblCambio.Location = new System.Drawing.Point(22, 152);
+            this.lblCambio.Location = new System.Drawing.Point(28, 193);
+            this.lblCambio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCambio.Name = "lblCambio";
-            this.lblCambio.Size = new System.Drawing.Size(51, 15);
+            this.lblCambio.Size = new System.Drawing.Size(53, 19);
             this.lblCambio.TabIndex = 5;
             this.lblCambio.Text = "Cambio";
             // 
             // txtCambio
             // 
-            this.txtCambio.Location = new System.Drawing.Point(20, 176);
+            this.txtCambio.Location = new System.Drawing.Point(26, 223);
+            this.txtCambio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCambio.Name = "txtCambio";
             this.txtCambio.ReadOnly = true;
-            this.txtCambio.Size = new System.Drawing.Size(100, 22);
+            this.txtCambio.Size = new System.Drawing.Size(127, 26);
             this.txtCambio.TabIndex = 4;
             // 
             // txtDinero
             // 
-            this.txtDinero.Location = new System.Drawing.Point(20, 120);
+            this.txtDinero.Location = new System.Drawing.Point(26, 152);
+            this.txtDinero.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtDinero.Name = "txtDinero";
-            this.txtDinero.Size = new System.Drawing.Size(140, 22);
+            this.txtDinero.Size = new System.Drawing.Size(179, 26);
             this.txtDinero.TabIndex = 3;
             // 
             // txtCosto
             // 
-            this.txtCosto.Location = new System.Drawing.Point(20, 49);
+            this.txtCosto.Location = new System.Drawing.Point(26, 62);
+            this.txtCosto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCosto.Name = "txtCosto";
-            this.txtCosto.Size = new System.Drawing.Size(100, 22);
+            this.txtCosto.Size = new System.Drawing.Size(127, 26);
             this.txtCosto.TabIndex = 2;
             // 
             // lblDinero
             // 
             this.lblDinero.AutoSize = true;
-            this.lblDinero.Location = new System.Drawing.Point(17, 88);
+            this.lblDinero.Location = new System.Drawing.Point(22, 111);
+            this.lblDinero.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDinero.Name = "lblDinero";
-            this.lblDinero.Size = new System.Drawing.Size(120, 15);
+            this.lblDinero.Size = new System.Drawing.Size(125, 19);
             this.lblDinero.TabIndex = 1;
             this.lblDinero.Text = "Cantidad de dinero:";
             // 
             // lblCosto
             // 
             this.lblCosto.AutoSize = true;
-            this.lblCosto.Location = new System.Drawing.Point(17, 31);
+            this.lblCosto.Location = new System.Drawing.Point(22, 39);
+            this.lblCosto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCosto.Name = "lblCosto";
-            this.lblCosto.Size = new System.Drawing.Size(113, 15);
+            this.lblCosto.Size = new System.Drawing.Size(123, 19);
             this.lblCosto.TabIndex = 0;
             this.lblCosto.Text = "Costo del Articulo:";
             // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Image = global::Calcular_el_cambio_según_una_compra.Properties.Resources.icons8_broom_48;
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpiar.Location = new System.Drawing.Point(337, 126);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(108, 52);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Image = global::Calcular_el_cambio_según_una_compra.Properties.Resources.icons8_add_48;
+            this.btnComprar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnComprar.Location = new System.Drawing.Point(337, 46);
+            this.btnComprar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(119, 56);
+            this.btnComprar.TabIndex = 6;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.ClientSize = new System.Drawing.Size(1200, 657);
             this.Controls.Add(this.gbPrincipal);
-            this.Font = new System.Drawing.Font("Britannic Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Harlow Solid Italic", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
             this.Text = "Calcular el cambio según una compra";
             this.gbPrincipal.ResumeLayout(false);
